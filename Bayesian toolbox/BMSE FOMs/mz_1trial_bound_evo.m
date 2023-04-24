335436879b539e9700195bc8fa0acb7c1b23f90a
Jesús Rubio <53397884+jesus-rubiojimenez@users.noreply.github.com>
2023-04-24 10:29:09 +0100

    Polished comments

--- Bayesian toolbox/BMSE FOMs/mz_1trial_bound_evo.m
+++ Bayesian toolbox/BMSE FOMs/mz_1trial_bound_evo.m
@@ -1,46 +1,32 @@
 function [bayes_bound] = mz_1trial_bound_evo(initial_state,phase_width,phase_mean)
-% Jesús Rubio Jiménez, PhD student
+% JesÃºs Rubio JimÃ©nez, PhD student
 % University of Sussex
 % 27th August 2018
 % J.Rubio-Jimenez@sussex.ac.uk
 %
 % mz_1trial_bound_evo(initial_state,phase_width,phase_mean),
 %
-% where 'initial_state' is the initial state of a Mach-Zehnder interferometer,
-% 'phase_width' is the width of a flat prior density function and 'phase_mean'
-% is the mean of the same flat prior.
+% where 'initial_state' is the initial state of a Mach-Zehnder interferometer,'phase_width' is the width of a flat prior and 'phase_mean' is the mean of such a
+% flat prior.
 %
-% Following the quantum Bayesian techniques developed by Helstrom and 
-% Personick, this programme calculates the value of the optimal Bayesian
-% mean square error 'bayes_bound' for 1 observation/trial/repetition.
+% Following the quantum Bayesian techniques developed by Helstrom and Personick, this programme calculates the value of the quantum minimum mean square error
+% ('bayes_bound') for 1 observation/trial/repetition.
 %
-% Note that mz_optimal_1trial_evo also calculated 
+% Note that mz_optimal_1trial_evo also calculates:
 %
-%   - the optimal Hermitian observable 'Lopt',
-%   - the possible estimates 'lambda' for the unknown parameter given by
-%     the spectrum of Lopt,
-%   - the optimal projective measurement for 1 observation/trial/repetition 
-%     given by the eigenvectors 'lambdavec_columns' of Lopt,
-%   - the zero-th moment of the transformed density matrix multiplied by
-%     the prior probability 'rho',
-%   - the diagonal zero-th moment of the transformed density matrix multiplied
-%     by the prior probability 'pk', 
-%   - the matrix 'psik' whose columns are the eigenvectors of rho,
-%     the first moment of the transformed density matrix multiplied by
-%     the prior probability 'rhobar',
-%   - the first moment of the transformed density matrix multiplied by
-%     the prior probability 'rhobarnew' expressed in the eigenbasis of rho.
+%   - the optimal quantum observable 'Lopt',
+%   - the possible estimates 'lambda' for the unknown parameter given by the spectrum of Lopt,
+%   - the optimal projective measurement for 1 observation/trial/repetition given by the eigenvectors 'lambdavec_columns' of Lopt,
+%   - the zero-th moment of the transformed density matrix multiplied by the prior probability 'rho',
+%   - the diagonal zero-th moment of the transformed density matrix multiplied by the prior probability 'pk', 
+%   - the matrix 'psik' whose columns are the eigenvectors of rho, the first moment of the transformed density matrix multiplied by the prior probability 'rhobar',
+%   - the first moment of the transformed density matrix multiplied by the prior probability 'rhobarnew' expressed in the eigenbasis of rho.
 %
-% These outputs are removed in order to increase the speed for the calculation 
-% of the optimal error itself, which is the only output that we need. 
+% These outputs are however removed in order to increase the speed, as the optimal error is the only output that we need. 
 
-% ADDED by Jesús Rubio on Jan 2021: for the theory behind the optimal quantum  
-% mean square error as calculated here, see
-%   * Rubio, J., and Dunningham, J. (2019). Quantum metrology in the presence
-%     of limited data. New Journal of Physics, 21 043037
-%   * Rubio Jiménez, J. (2020). Non-asymptotic quantum metrology: extracting
-%     maximum information from limited data. PhD thesis, University of Sussex,
-%     ISNI: 0000 0004 8504 6357 (arXiv:1912.02324).
+% ADDED by JesÃºs Rubio (Jan 2021): for the theory behind the calculation scheme used here, see:
+%   [1] Rubio, J., and Dunningham, J. (2019). Quantum metrology in the presence of limited data. New Journal of Physics, 21 043037
+%   [2] Rubio JimÃ©nez, J. (2020). Non-asymptotic quantum metrology: extracting maximum information from limited data. PhD thesis, University of Sussex, ISNI: 0000 0004 8504 6357 (arXiv:1912.02324).
 
 % Bayes statistical and mean operators
 index=1;
